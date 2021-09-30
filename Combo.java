24b5052fe04a77296e279fa77af64507b80de570
Matheus Pilon <matheuspilon.m@gmail.com>
2021-09-29 22:16:26 -0300

    atualização

--- Combo.java
+++ Combo.java
@@ -9,38 +9,26 @@ class Combo {
 	
 	public void setLanche(String Lanche) {
 		this.Lanche = Lanche;
-		
 	}
-	
 	public void setBatata(String Batata) {
 		this.Batata = Batata;
-		
 	}
-	
 	public void setBebida(String Bebida) {
 		this.Bebida = Bebida;
-		
 	}
-	
 	public void setPreco(double Preco) {
 		this.Preco = Preco;
-		
 	}
-	
 	public String getLanche() {
 		return Lanche;
 	}
-	
 	public String getBatata() {
 		return Batata;
 	}
-	
 	public String getBebida() {
 		return Bebida;
 	}
-	
 	public double getPreco() {
 		return Preco;
 	}
-
 }
